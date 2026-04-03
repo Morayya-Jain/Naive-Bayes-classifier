a31c6fdc5d95ddcc06af430e550eaf96c3d09ec5
morayyajain <morayyajain@gmail.com>
2026-04-03 16:12:06 +1100

    major functions

--- evaluate.py
+++ evaluate.py
@@ -2,10 +2,11 @@
 
 import numpy as np
 import pandas as pd
+from config import CLASSES
 
 
 def compute_confusion_matrix(y_true, y_pred):
-    """Compute a 2x2 confusion matrix.
+    """Compute a confusion matrix.
 
     Convention (matching the spec):
         Positive class = '>50K'
@@ -18,7 +19,24 @@ def compute_confusion_matrix(y_true, y_pred):
     Returns:
         Dict with keys 'TP', 'FP', 'TN', 'FN'.
     """
-    pass
+
+    confusion_matrix = {'TP': 0, 'FP': 0, 'TN': 0, 'FN': 0}
+
+    for true, pred in zip(y_true, y_pred):
+        # Negative class
+        if true == CLASSES[0]:
+            if pred == true:
+                confusion_matrix['TN'] += 1
+            else:
+                confusion_matrix['FP'] += 1
+        # Positive class
+        else:
+            if pred == true:
+                confusion_matrix['TP'] += 1
+            else:
+                confusion_matrix['FN'] += 1
+    
+    return confusion_matrix
 
 
 def compute_metrics(y_true, y_pred):
@@ -33,7 +51,30 @@ def compute_metrics(y_true, y_pred):
         {'accuracy': ..., '<=50K': {'precision': ..., 'recall': ..., 'f1': ...},
          '>50K': {'precision': ..., 'recall': ..., 'f1': ...}}.
     """
-    pass
+    
+    confusion_matrix = compute_confusion_matrix(y_true, y_pred)
+    TP, FP = confusion_matrix['TP'], confusion_matrix['FP']
+    TN, FN = confusion_matrix['TN'], confusion_matrix['FN']
+
+    correct_prediction = 0
+    for true, pred in zip(y_true, y_pred):
+        correct_prediction += (true == pred)
+    accuracy = correct_prediction / len(y_pred) if len(y_pred) > 0 else -1
+
+    # For '>50K' class label
+    positive_precision = TP / (TP + FP)
+    positive_recall = TP / (TP + FN)
+    positive_f1 = (2 * positive_precision * positive_recall) / (positive_precision + positive_recall)
+    positive_class_metrics = {'precision': positive_precision, 'recall': positive_recall, 'f1': positive_f1}
+    
+    # For '<=50K' class label
+    negative_precision = TN / (TN + FN)
+    negative_recall = TN / (TN + FP)
+    negative_f1 = (2 * negative_precision * negative_recall) / (negative_precision + negative_recall)
+    negative_class_metrics = {'precision': negative_precision, 'recall': negative_recall, 'f1': negative_f1}
+
+    metrics = {'accuracy': accuracy, '<=50K': negative_class_metrics, '>50K': positive_class_metrics}
+    return metrics
 
 
 def posterior_ratio(log_proba):
@@ -55,7 +96,13 @@ def posterior_ratio(log_proba):
     Returns:
         Array of posterior ratios (one per instance).
     """
-    pass
+    
+    negative = log_proba[:, 0]  # '<=50K'
+    positive = log_proba[:, 1]  # '>50K'
+    log_ratio = positive - negative
+    
+    ratio = np.exp(log_ratio)
+    return ratio
 
 
 def find_high_confidence(X, log_proba, y_true, class_label, n=5):
